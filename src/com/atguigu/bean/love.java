b28c29ac6e6e72e0f50b3595bd05582f78176fe9
ming39 <yangmingjiu37@163.com>
2020-04-17 16:20:34 +0800

    111

--- src/com/atguigu/bean/love.java
+++ src/com/atguigu/bean/love.java
@@ -2,7 +2,7 @@ package com.atguigu.bean;
 
 public class love {
 	public static void main(String[] args) {
-		System.out.println("i love you lisi");
+		System.out.println("i love you lisi111");
 		System.out.println("i love you too");
 	}
 }
