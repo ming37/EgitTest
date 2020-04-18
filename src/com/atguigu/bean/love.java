cb4b384b3009b71d2aa36b91cf84b62382eb3edb
ming39 <yangmingjiu37@163.com>
2020-04-18 10:23:11 +0800

    合并

--- src/com/atguigu/bean/love.java
+++ src/com/atguigu/bean/love.java
@@ -4,6 +4,6 @@ public class love {
 	public static void main(String[] args) {
 		System.out.println("i love you too222");
 		System.out.println("i love you lisi111");
-
+		System.out.println("ºÏ²¢");
 	}
 }
